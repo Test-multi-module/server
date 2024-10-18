d136022548fc0ba472c7adf78b86b995c1a28a06
Yana <yana.tereshchenko@softpos.eu>
2024-10-18 12:04:35 +0200

    mvn-clean-install + application start completed successfully

--- db/db-schema/src/main/java/com/db/schema/config/DatabaseConfig.java
+++ db/db-schema/src/main/java/com/db/schema/config/DatabaseConfig.java
@@ -1,25 +1,37 @@
 package com.db.schema.config;
 
+import javax.sql.DataSource;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
 
+    @Value("${spring.datasource.url:jdbc:postgresql://localhost:5432/postgres}")
+    private String dataSourceUrl;
+    //jdbc:postgresql://auction4.postgres.database.azure.com:5432/postgres?sslmode=require
+
+    @Value("${spring.datasource.username:adm}")
+    private String dataSourceUserName;
+
+    @Value("${spring.datasource.password:24o62o!!}")
+    private String dataSourcePassword;
+
     @Bean
     public DataSource dataSource() {
-        return DataSourceBuilder.create()
-                .url("jdbc:postgresql://auction4.postgres.database.azure.com:5432/postgres?sslmode=require")
-                .username("adm")
-                .password("24o62o!!")
-                .driverClassName("org.postgresql.Driver")
-                .build();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(dataSourceUrl);
+        dataSource.setUsername(dataSourceUserName);
+        dataSource.setPassword(dataSourcePassword);
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        return dataSource;
     }
 
     @Bean
--- db/db-schema/src/main/java/com/db/schema/config/FlywayConfig.java
+++ db/db-schema/src/main/java/com/db/schema/config/FlywayConfig.java
@@ -3,13 +3,19 @@ package com.db.schema.config;
 import org.flywaydb.core.Flyway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 import javax.sql.DataSource;
 
 @Configuration
+@DependsOn("databaseConfig")
 public class FlywayConfig {
-    @Bean
+    @Bean(initMethod = "migrate")
     public Flyway flyway(DataSource dataSource) {
-        return Flyway.configure().dataSource(dataSource).load();
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .baselineOnMigrate(true)
+                .locations("classpath:db/migration")
+                .load();
     }
 }
